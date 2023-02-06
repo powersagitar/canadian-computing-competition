a03752969713986671f82222d43e3f9c7a9bf732
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2023-02-06 15:02:47 -0500

    progress

---
+++ 2021/J5.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    // get input
+    unsigned m = 0, n = 0;
+    std::cin >> m;
+    std::cin >> n;
+
+    std::vector<std::string> input;
+    unsigned inputCount = 0;
+    std::cin >> inputCount;
+    for (unsigned i = 0; i < inputCount; i++)
+    {
+        std::string inputln = "";
+        do
+        {
+            std::getline(std::cin, inputln);
+        } while (inputln.empty());
+        input.push_back(inputln);
+    }
+
+    // main process
+    // initialize bool array to all false
+    bool canvas[m][n];
+    for (size_t r = 0; r < m; r++)
+    {
+        for (size_t c = 0; c < n; c++)
+        {
+            canvas[r][c] = false;
+        }
+    }
+
+    // change color
+    for (const std::string &inputln : input)
+    {
+        // use 0 based indexing
+        unsigned number = std::stoul(inputln.substr(2)) - 1;
+
+        switch (inputln[0])
+        {
+            case 'R':
+                for (unsigned c = 0; c < m; c++)
+                {
+                    canvas[number][c] = !canvas[number][c];
+                }
+                break;
+
+            case 'C':
+                for (unsigned r = 0; r < n; r++)
+                {
+                    canvas[r][number] = !canvas[r][number];
+                }
+                break;
+        }
+    }
+
+    // count gold blocks
+    unsigned goldCount = 0;
+    for (unsigned r = 0; r < m; r++)
+    {
+        for (unsigned c = 0; c < n; c++)
+        {
+            if (canvas[r][c])
+            {
+                goldCount++;
+            }
+        }
+    }
+
+    std::cout << goldCount;
+
+    return 0;
+}
